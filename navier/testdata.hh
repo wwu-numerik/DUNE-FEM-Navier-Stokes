2ae1289a267309ffd67394f574094cf5f97587de
Rene Milk <rene.milk@uni-muenster.de>
2010-04-20 17:52:47 +0200

    real function eval for test dirichlet data

--- navier/testdata.hh
+++ navier/testdata.hh
@@ -80,8 +80,12 @@ namespace Dune {
 					   *
 					   *  doing nothing besides Base init
 					   **/
-					  DirichletData( const FunctionSpaceImp& space )
-						  : BaseType( space )
+					  DirichletData( const FunctionSpaceImp& space,
+									 const double parameter_a = M_PI /2.0 ,
+									 const double parameter_d = M_PI /4.0)
+						  : BaseType( space ),
+						  parameter_a_( parameter_a ),
+						  parameter_d_( parameter_d )
 					  {}
 
 					  /**
@@ -93,10 +97,27 @@ namespace Dune {
 					   {}
 
 					  template < class IntersectionType >
-					  void evaluate( const double time, const DomainType& arg, RangeType& ret, const IntersectionType& intersection ) const
+					  void evaluate( const double time, const DomainType& arg, RangeType& ret, const IntersectionType& /*intersection */) const
 					  {
-						  const int id = intersection.boundaryId();
 						  Dune::CompileTimeChecker< ( dim_ == 3 ) > DirichletData_Unsuitable_WorldDim;
+						  const double x		= arg[0];
+						  const double y		= arg[1];
+						  const double z		= arg[2];
+						  const double e_d_d_t	= std::exp( -1 * parameter_d_*parameter_d_ *  time );
+						#define PM +
+						  ret[0] = - parameter_a_ * (
+								  ( std::exp( parameter_a_ * x ) * std::sin( parameter_a_ * y PM/*+-*/ parameter_d_ * z ) ) +
+								  ( std::exp( parameter_a_ * z ) * std::cos( parameter_a_ * x PM/*+-*/ parameter_d_ * y ) )
+								) * e_d_d_t;
+						  ret[1] = - parameter_a_ * (
+								  ( std::exp( parameter_a_ * y ) * std::sin( parameter_a_ * z PM/*+-*/ parameter_d_ * x ) ) +
+								  ( std::exp( parameter_a_ * x ) * std::cos( parameter_a_ * y PM/*+-*/ parameter_d_ * z ) )
+								) * e_d_d_t;
+						  ret[2] = - parameter_a_ * (
+								  ( std::exp( parameter_a_ * z ) * std::sin( parameter_a_ * x PM/*+-*/ parameter_d_ * y ) ) +
+								  ( std::exp( parameter_a_ * y ) * std::cos( parameter_a_ * z PM/*+-*/ parameter_d_ * x ) )
+								) * e_d_d_t;
+						#undef PM
 					  }
 
 					   /**
@@ -110,6 +131,8 @@ namespace Dune {
 
 				  private:
 					  static const int dim_ = FunctionSpaceImp::dimDomain ;
+					  const double parameter_a_;
+					  const double parameter_d_;
 				};
 
 		}//end namespace TestCase
