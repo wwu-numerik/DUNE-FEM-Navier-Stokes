878b72796052a433d0337496006f65678a229436
Rene Milk <rene.milk@uni-muenster.de>
2010-04-28 15:15:42 +0200

    - thetascheme run fixed for new timeprovider functionality - added datawriter

--- navier/metadata.hh
+++ navier/metadata.hh
@@ -6,6 +6,7 @@
 #include <dune/navier/fractionaltimeprovider.hh>
 #include <dune/navier/stokestraits.hh>
 #include <dune/fem/misc/mpimanager.hh>
+#include <dune/stuff/datawriter.hh>
 #include <dune/common/collectivecommunication.hh>
 #include <cmath>
 
@@ -13,8 +14,8 @@ namespace Dune {
 	namespace NavierStokes {
 		template <	class CommunicatorImp,
 					class GridPartImp,
-					template <class > class AnalyticalForceImp,
-					template <class > class AnalyticalDirichletDataImp,
+					template < class > class AnalyticalForceImp,
+					template < class > class AnalyticalDirichletDataImp,
 					int gridDim, int sigmaOrder, int velocityOrder = sigmaOrder, int pressureOrder = sigmaOrder >
 		struct ThetaSchemeTraits {
 			typedef GridPartImp
@@ -51,11 +52,16 @@ namespace Dune {
 
 			typedef CommunicatorImp
 					CommunicatorType;
+
+			typedef TimeAwareDataWriter<	TimeProviderType,
+								typename GridPartType::GridType,
+								typename DiscreteStokesFunctionWrapperType::Traits::FunctionTupleType >
+					DataWriterType;
 		};
 
 		template < class TraitsImp >
 		class ThetaScheme {
-			public:
+			protected:
 				typedef TraitsImp
 						Traits;
 				typedef typename Traits::CommunicatorType
@@ -64,11 +70,13 @@ namespace Dune {
 				const double theta_;
 				const double operator_weight_alpha_;
 				const double operator_weight_beta_;
-				const double deltaTime_;
 				typename Traits::GridPartType gridPart_;
-				typename Traits::DiscreteStokesFunctionSpaceWrapperType functionSpaceWrapper_;
-
 				typename Traits::TimeProviderType timeprovider_;
+				typename Traits::DiscreteStokesFunctionSpaceWrapperType functionSpaceWrapper_;
+				typename Traits::DiscreteStokesFunctionWrapperType currentFunctions_;
+				typename Traits::DiscreteStokesFunctionWrapperType nextFunctions_;
+				typename Traits::DataWriterType dataWriter_;
+
 			public:
 				ThetaScheme( typename Traits::GridPartType gridPart,
 							 const double theta = 1 - std::pow( 2.0, -1/2.0 ),
@@ -79,27 +87,25 @@ namespace Dune {
 					operator_weight_alpha_( ( 1-2*theta_ ) / ( 1-theta_ ) ),
 					operator_weight_beta_( 1 - operator_weight_alpha_ ),
 					communicator_( comm ),
-					deltaTime_( Parameters().getParam( "deltaTime", 1e-2 ) ),
-		//			const double startTime	= Parameters().getParam( "startTime", 0.0 ),
-					timeprovider_( deltaTime_, theta_, communicator_ ),
-					functionSpaceWrapper_( gridPart_ )
+					timeprovider_( theta_,operator_weight_alpha_,operator_weight_beta_, communicator_ ),
+					functionSpaceWrapper_( gridPart_ ),
+					currentFunctions_(  "current_",
+										functionSpaceWrapper_,
+										gridPart_ ),
+					nextFunctions_(  "next_",
+									functionSpaceWrapper_,
+									gridPart_ ),
+					dataWriter_( timeprovider_, gridPart_.grid(), nextFunctions_.functionTuple() )
 				{
 
 				}
 
 				void run()
 				{
-					typename Traits::DiscreteStokesFunctionWrapperType
-						currentFunctions(  "current_",
-											functionSpaceWrapper_,
-											gridPart_ );
-					typename Traits::DiscreteStokesFunctionWrapperType
-						nextFunctions(  "next_",
-											functionSpaceWrapper_,
-											gridPart_ );
+					currentFunctions_.clear(); //actually load initial velocity/pressure
 					const double viscosity = 48102.;
 					const double alpha = 48102.;
-					typename Traits::AnalyticalForceType stokesForce( timeprovider_, currentFunctions.discreteVelocity() );
+					typename Traits::AnalyticalForceType stokesForce( timeprovider_, currentFunctions_.discreteVelocity() );
 					typename Traits::AnalyticalDirichletDataType stokesDirichletData =
 							Traits::StokesModelTraits::AnalyticalDirichletDataTraitsImplementation
 											::getInstance( timeprovider_,functionSpaceWrapper_ );
@@ -111,20 +117,17 @@ namespace Dune {
 										alpha );
 					typename Traits::StokesStartPassType stokesStartPass;
 					typename Traits::StokesPassType stokesPass( stokesStartPass,
-											stokesModel,//reference goes out-of-scope??
+											stokesModel,
 											gridPart_,
 											functionSpaceWrapper_ );
 
-
-					timeprovider_.provideCflEstimate( 1 );
-					//not manually setting the delta in tp.nexxt() results in assertions cause TimepRoiver claims dt isn't valid ie unset..
-					const double endTime	= Parameters().getParam( "endTime", 1.0 );
-					for( timeprovider_.init( deltaTime_ ); timeprovider_.time() < endTime; )
+					for( timeprovider_.init( timeprovider_.deltaT() ); timeprovider_.time() < timeprovider_.endTime(); )
 					{
 						for ( unsigned int i =0 ; i< 3 ; ++i, timeprovider_.nextFractional() )
 							std::cout << "current time (substep " << i << "): " << timeprovider_.subTime() << std::endl;
+						dataWriter_.write();
 					}
-					stokesPass.apply(currentFunctions,nextFunctions);
+//					stokesPass.apply(currentFunctions_,nextFunctions_);
 				}
 		};
 	}//end namespace NavierStokes
