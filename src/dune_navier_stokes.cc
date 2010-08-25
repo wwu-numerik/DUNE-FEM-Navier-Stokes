2e795b7f7b0c9e51a8c95dc7d254b29100b6862c
Rene Milk <rene.milk@uni-muenster.de>
2010-08-25 10:25:13 +0200

    make navier always set the param to use reduced oseen solver

--- src/dune_navier_stokes.cc
+++ src/dune_navier_stokes.cc
@@ -271,6 +271,7 @@ RunInfoVector singleRun(  CollectiveCommunication& mpicomm,
 	const double grid_width = Dune::GridWidth::calcGridWidth( gridPart );
 	infoStream << "  - max grid width: " << grid_width << std::endl;
 
+	Parameters().setParam( "reduced_oseen_solver", true );
 	Dune::NavierStokes::ThetaScheme<ThetaSchemeTraitsType>
 			thetaScheme( gridPart );
 	runInfoVector = thetaScheme.run();
