c4685c34d54b043451ca33de7e3e4f08a653f81c
Felix Tobias Albrecht <falbr_01@BARNEY.(none)>
2010-09-22 14:47:33 +0200

    2D tube testcase

--- navier/testdata.hh
+++ navier/testdata.hh
@@ -619,7 +619,315 @@ namespace Dune {
 
 		}//end namespace TestCase2D
 
-		namespace TrivialTestCase {
+                namespace TestCase2DAnimation {
+                        static const double pi_factor = M_PI;//controls number of vortices
+                        template < class FunctionSpaceImp >
+                        class Force : public Function < FunctionSpaceImp , Force < FunctionSpaceImp > >
+                        {
+                                  public:
+                                          typedef Force< FunctionSpaceImp >
+                                                  ThisType;
+                                          typedef Function < FunctionSpaceImp ,ThisType >
+                                                  BaseType;
+                                          typedef typename BaseType::DomainType
+                                                  DomainType;
+                                          typedef typename BaseType::RangeType
+                                                  RangeType;
+
+                                          /**
+                                           *  \brief  constructor
+                                           *  \param  viscosity   viscosity \f$\mu\f$ of the fluid
+                                           **/
+                                          Force( const double viscosity, const FunctionSpaceImp& space, const double alpha = 0.0 )
+                                                  : BaseType ( space ),
+                                                        viscosity_( viscosity ),
+                                                        alpha_( alpha )
+                                          {}
+
+                                          /**
+                                           *  \brief  destructor
+                                           *  doing nothing
+                                           **/
+                                          ~Force()
+                                          {}
+
+                                          /**
+                                           *  \brief  evaluates the force
+                                           *  \param  arg
+                                           *          point to evaluate at
+                                           *  \param  ret
+                                           *          value of force at given point
+                                           **/
+                                          inline void evaluate( const double time, const DomainType& arg, RangeType& ret ) const
+                                          {
+                                                  ret = RangeType(0);
+                                          }
+                                          inline void evaluate( const DomainType& /*arg*/, RangeType& ret ) const {assert(false);}
+
+                                  private:
+                                          const double viscosity_;
+                                          const double alpha_;
+                                          static const int dim_ = FunctionSpaceImp::dimDomain;
+                        };
+
+                        /**
+                        *  \brief  describes the dirichlet boundary data
+                        *
+                        *  \tparam DirichletTraitsImp
+                        *          types like functionspace, range type, etc
+                        *
+                        *  \todo   extensive docu with latex
+                        **/
+                        template < class FunctionSpaceImp >
+                        class DirichletData : public Function < FunctionSpaceImp , DirichletData < FunctionSpaceImp > >
+                        {
+                                public:
+                                        typedef DirichletData< FunctionSpaceImp >
+                                                ThisType;
+                                        typedef Function< FunctionSpaceImp, ThisType >
+                                                BaseType;
+                                        typedef typename BaseType::DomainType
+                                                DomainType;
+                                        typedef typename BaseType::RangeType
+                                                RangeType;
+
+                                        /**
+                                        *  \brief  constructor
+                                        *
+                                        *  doing nothing besides Base init
+                                        **/
+                                        DirichletData( const FunctionSpaceImp& space,
+                                                                 const double parameter_a = M_PI /2.0 ,
+                                                                 const double parameter_d = M_PI /4.0)
+                                                : BaseType( space ),
+                                                parameter_a_( parameter_a ),
+                                                parameter_d_( parameter_d )
+                                        {}
+
+                                        /**
+                                        *  \brief  destructor
+                                        *
+                                        *  doing nothing
+                                        **/
+                                        ~DirichletData()
+                                        {}
+
+                                        template < class IntersectionType >
+                                        void evaluate( const double time, const DomainType& arg, RangeType& ret, const IntersectionType& intersection) const
+                                        {
+                                                Dune::CompileTimeChecker< ( dim_ == 2 ) > DirichletData_Unsuitable_WorldDim;
+                                                const int id = intersection.boundaryId();
+                                                const double y = arg[1];
+                                                const double f = -4 * ( y - 0.5) * ( y + 0.5) ;
+                                                ret = RangeType( 0.0 );
+                                                if ( id == 2 ) { // bottom
+                                                    ret[ 0 ] = 0.0;
+                                                    ret[ 1 ] = 0.0;
+                                                }
+                                                else if ( id == 3 ) { // right
+                                                    ret[ 0 ] = std::abs( std::sin( 2.0 * M_PI * time ) ) * f;
+                                                    ret[ 1 ] = 0.0;
+                                                }
+                                                else if ( id == 4 ) { // top
+                                                    ret[ 0 ] = 0.0;
+                                                    ret[ 1 ] = 0.0;
+                                                }
+                                                else if ( id == 5 ) { // left
+                                                    ret[ 0 ] = std::abs( std::sin( 2.0 * M_PI * time ) ) * f;
+                                                    ret[ 1 ] = 0.0;
+                                                }
+                                        }
+
+                                        /**
+                                        * \brief  evaluates the dirichlet data
+                                        * \param  arg
+                                        *         point to evaluate at
+                                        * \param  ret
+                                        *         value of dirichlet boundary data at given point
+                                        **/
+                                        inline void evaluate( const DomainType& arg, RangeType& ret ) const {assert(false);}
+
+                                private:
+                                          static const int dim_ = FunctionSpaceImp::dimDomain ;
+                                          const double parameter_a_;
+                                          const double parameter_d_;
+                        };
+
+                        template < class FunctionSpaceImp, class TimeProviderImp >
+                        class Velocity : public TimeFunction < FunctionSpaceImp , Velocity< FunctionSpaceImp,TimeProviderImp >, TimeProviderImp >
+                        {
+                                public:
+                                        typedef Velocity< FunctionSpaceImp, TimeProviderImp >
+                                                ThisType;
+                                        typedef TimeFunction< FunctionSpaceImp, ThisType, TimeProviderImp >
+                                                BaseType;
+                                        typedef typename BaseType::DomainType
+                                                DomainType;
+                                        typedef typename BaseType::RangeType
+                                                RangeType;
+
+                                        /**
+                                        *  \brief  constructor
+                                        *
+                                        *  doing nothing besides Base init
+                                        **/
+                                        Velocity(	const TimeProviderImp& timeprovider,
+                                                                const FunctionSpaceImp& space,
+                                                                const double parameter_a = M_PI /2.0 ,
+                                                                const double parameter_d = M_PI /4.0)
+                                                : BaseType( timeprovider, space ),
+                                                parameter_a_( parameter_a ),
+                                                parameter_d_( parameter_d )
+                                        {}
+
+                                        /**
+                                        *  \brief  destructor
+                                        *
+                                        *  doing nothing
+                                        **/
+                                        ~Velocity()
+                                        {}
+
+                                        void evaluateTime( const double time, const DomainType& arg, RangeType& ret ) const
+                                        {
+                                                Dune::CompileTimeChecker< ( dim_ == 2 ) > DirichletData_Unsuitable_WorldDim;
+                                                ret = RangeType( 0.0 );
+                                        }
+
+                                   /**
+                                        * \brief  evaluates the dirichlet data
+                                        * \param  arg
+                                        *         point to evaluate at
+                                        * \param  ret
+                                        *         value of dirichlet boundary data at given point
+                                        **/
+//					inline void evaluate( const DomainType& arg, RangeType& ret ) const {assert(false);}
+
+                                private:
+                                        static const int dim_ = FunctionSpaceImp::dimDomain ;
+                                        const double parameter_a_;
+                                        const double parameter_d_;
+                        };
+
+                        template <	class FunctionSpaceImp,
+                                                class TimeProviderImp >
+                        class Pressure : public TimeFunction <	FunctionSpaceImp ,
+                                                                                                        Pressure < FunctionSpaceImp,TimeProviderImp >,
+                                                                                                        TimeProviderImp >
+                        {
+                                public:
+                                        typedef Pressure< FunctionSpaceImp, TimeProviderImp >
+                                                ThisType;
+                                        typedef TimeFunction< FunctionSpaceImp, ThisType, TimeProviderImp >
+                                                BaseType;
+                                        typedef typename BaseType::DomainType
+                                                DomainType;
+                                        typedef typename BaseType::RangeType
+                                                RangeType;
+
+                                  /**
+                                   *  \brief  constructor
+                                   *
+                                   *  doing nothing besides Base init
+                                   **/
+                                  Pressure( const TimeProviderImp& timeprovider,
+                                                        const FunctionSpaceImp& space,
+                                                        const double parameter_a = M_PI /2.0 ,
+                                                        const double parameter_d = M_PI /4.0)
+                                          : BaseType( timeprovider, space ),
+                                          parameter_a_( parameter_a ),
+                                          parameter_d_( parameter_d )
+                                  {}
+
+                                  /**
+                                   *  \brief  destructor
+                                   *
+                                   *  doing nothing
+                                   **/
+                                   ~Pressure()
+                                   {}
+
+                                        void evaluateTime( const double time, const DomainType& arg, RangeType& ret ) const
+                                        {
+                                            ret = RangeType( 0.0 );
+                                        }
+
+                                        /**
+                                        * \brief  evaluates the dirichlet data
+                                        * \param  arg
+                                        *         point to evaluate at
+                                        * \param  ret
+                                        *         value of dirichlet boundary data at given point
+                                        **/
+//					inline void evaluate( const DomainType& arg, RangeType& ret ) const {assert(false);}
+
+                                private:
+                                        static const int dim_ = FunctionSpaceImp::dimDomain ;
+                                        const double parameter_a_;
+                                        const double parameter_d_;
+                        };
+
+                        template <	class FunctionSpaceImp,
+                                                class TimeProviderImp >
+                        class PressureGradient : public TimeFunction <	FunctionSpaceImp ,
+                                                                                                        PressureGradient < FunctionSpaceImp,TimeProviderImp >,
+                                                                                                        TimeProviderImp >
+                        {
+                                public:
+                                        typedef PressureGradient< FunctionSpaceImp, TimeProviderImp >
+                                                ThisType;
+                                        typedef TimeFunction< FunctionSpaceImp, ThisType, TimeProviderImp >
+                                                BaseType;
+                                        typedef typename BaseType::DomainType
+                                                DomainType;
+                                        typedef typename BaseType::RangeType
+                                                RangeType;
+
+                                  /**
+                                   *  \brief  constructor
+                                   *
+                                   *  doing nothing besides Base init
+                                   **/
+                                  PressureGradient( const TimeProviderImp& timeprovider,
+                                                        const FunctionSpaceImp& space,
+                                                        const double parameter_a = M_PI /2.0 ,
+                                                        const double parameter_d = M_PI /4.0)
+                                          : BaseType( timeprovider, space ),
+                                          parameter_a_( parameter_a ),
+                                          parameter_d_( parameter_d )
+                                  {}
+
+                                  /**
+                                   *  \brief  destructor
+                                   *
+                                   *  doing nothing
+                                   **/
+                                   ~PressureGradient()
+                                   {}
+
+                                    void evaluateTime( const double time, const DomainType& arg, RangeType& ret ) const
+                                    {
+                                        ret = RangeType( 0.0 );
+                                    }
+
+                                        /**
+                                        * \brief  evaluates the dirichlet data
+                                        * \param  arg
+                                        *         point to evaluate at
+                                        * \param  ret
+                                        *         value of dirichlet boundary data at given point
+                                        **/
+//					inline void evaluate( const DomainType& arg, RangeType& ret ) const {assert(false);}
+
+                                private:
+                                        static const int dim_ = FunctionSpaceImp::dimDomain ;
+                                        const double parameter_a_;
+                                        const double parameter_d_;
+                        };
+
+                }//end namespace TestCase2D
+
+                namespace TrivialTestCase {
 			template < class FunctionSpaceImp >
 			class Force : public Function < FunctionSpaceImp , Force < FunctionSpaceImp > >
 			{
