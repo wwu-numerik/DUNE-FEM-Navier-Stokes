dccd9c64641900ad814ec60715a2fb04ca1a5cbe
Rene Milk <rene.milk@uni-muenster.de>
2010-11-25 00:47:01 +0100

    new rhs adapter

--- navier/rhsadapter.hh
+++ navier/rhsadapter.hh
@@ -221,6 +221,129 @@ namespace Dune {
 					}
 			};
 		}//end namespace NonlinearStep
+		namespace OseenStep {
+		/** \brief take previous step solution \f$u_{k-1}\f$ and analytical RHS to form function to be passed to either StokesStep
+		  * given analytical force \f$f_{ana}\f$ and discrete function \f$u\f$ representing previous time step's velocity solution,
+		  *	this calculates new right hand side \f$F := \theta_{3} \delta{t_n} f_{k} + \theta_{2} \delta{t_n}f_{k-1}
+									+ u_{k-1} + \frac{\theta_{1} \delta{t_n}}{Re} \Delta u_{k-1}
+									 + \theta_{1} \delta{t_n} - \left( _{k-1} \cdot \nabla \right) _{k-1} \f$
+		  * \note theta value array is 0 based, so all indices have a -1 offset to the paper
+		  */
+		template <	class TimeProviderType,
+					class AnalyticalForceType,
+					class DiscreteVelocityFunctionType,
+					class ThetaValuesType >
+		class ForceAdapterFunction :
+				public DiscreteVelocityFunctionType
+		{
+			protected:
+				typedef ForceAdapterFunction<	TimeProviderType,
+												AnalyticalForceType,
+												DiscreteVelocityFunctionType,
+												ThetaValuesType >
+					ThisType;
+				const TimeProviderType& timeProvider_;
+				const AnalyticalForceType& force_;
+				const double reynolds_;
+				const ThetaValuesType& theta_values_;
+
+			public:
+				typedef DiscreteVelocityFunctionType
+					BaseType;
+
+				//! this sginature is used in the first stokes where we have analytical data to derive from
+				ForceAdapterFunction(	const TimeProviderType& timeProvider,
+										const DiscreteVelocityFunctionType& velocity,
+										const AnalyticalForceType& force,
+										const double reynolds,
+										const ThetaValuesType& theta_values,
+										int polOrd = -1 )
+					: BaseType( "stokes-ana-rhsdapater" , velocity.space()),
+					timeProvider_( timeProvider ),
+					force_( force ),
+					reynolds_( reynolds ),
+					theta_values_( theta_values )
+				{
+
+					//						TESTING_NS
+					typedef typename DiscreteVelocityFunctionType::FunctionSpaceType::FunctionSpaceType
+						VelocityFunctionSpaceType;
+					VelocityFunctionSpaceType continousVelocitySpace_;
+
+					typedef TESTING_NS::VelocityLaplace<	VelocityFunctionSpaceType,
+															TimeProviderType >
+							VelocityLaplace;
+					VelocityLaplace velocity_laplace( timeProvider_, continousVelocitySpace_ );
+					typedef TESTING_NS::VelocityConvection<	VelocityFunctionSpaceType,
+															TimeProviderType >
+						VelocityConvection;
+					VelocityConvection velocity_convection( timeProvider_, continousVelocitySpace_ );
+
+					DiscreteVelocityFunctionType velocity_convection_discrete("velocity_convection_discrete", velocity.space() );
+					DiscreteVelocityFunctionType velocity_laplace_discrete("velocity_laplace_discrete", velocity.space() );
+
+					Dune::BetterL2Projection //we need evals from the _previous_ (t_{k-1}) step
+						::project( timeProvider_.previousSubTime(), velocity_convection, velocity_convection_discrete );
+					Dune::BetterL2Projection
+						::project( timeProvider_.previousSubTime(), velocity_laplace, velocity_laplace_discrete );
+
+					AddCommon( velocity, velocity_convection_discrete, velocity_laplace_discrete, theta_values_ );
+				}
+
+				//! this signature is used in all other stokes steps where we get the data from the previous step's discretisation
+				template < class RhsContainerType >
+				ForceAdapterFunction(	const TimeProviderType& timeProvider,
+										const DiscreteVelocityFunctionType& velocity,
+										const AnalyticalForceType& force,
+										const double reynolds,
+										const RhsContainerType& rhs_container,
+										const ThetaValuesType& theta_values,
+										int polOrd = -1 )
+					: BaseType( "stokes-ana-rhsdapater" , velocity.space()),
+					timeProvider_( timeProvider ),
+					force_( force ),
+					reynolds_( reynolds ),
+					theta_values_( theta_values )
+				{
+					AddCommon( velocity, rhs_container.convection, rhs_container.velocity_laplace );
+				}
+
+			protected:
+				/** \f$F := \theta_{3} \delta{t_n} f_{k} + \theta_{2} \delta{t_n}f_{k-1}
+				+ u_{k-1} + \frac{\theta_{1} \delta{t_n}}{Re} \Delta u_{k-1}
+				 + \theta_{1} \delta{t_n} - \left( _{k-1} \cdot \nabla \right) _{k-1} \f$
+				\note theta value array is 0 based, so all indices have a -1 offset to the paper**/
+				void AddCommon( const DiscreteVelocityFunctionType& velocity,
+								const DiscreteVelocityFunctionType& convection,
+								const DiscreteVelocityFunctionType& velocity_laplace )
+				{
+					const double dt_n = timeProvider_.deltaT();
+
+					DiscreteVelocityFunctionType tmp("rhs-ana-tmp", velocity.space() );
+					Dune::BetterL2Projection
+						::project( timeProvider_, force_, tmp );
+					tmp *= ( theta_values_[3] * dt_n );
+					*this += tmp;
+
+					Dune::BetterL2Projection
+						::project( timeProvider_.previousSubTime(), force_, tmp );
+					tmp *= ( theta_values_[2] * dt_n );
+					*this += tmp;
+
+					tmp.assign( velocity_laplace );
+					tmp *= ( theta_values_[1] * dt_n ) / reynolds_;
+					*this += tmp;// this = f + beta_re_qoutient * laplace
+
+					tmp.assign( convection );
+					tmp *= ( theta_values_[1] * dt_n )
+					*this -= tmp;
+
+					*this += velocity;
+				}
+
+
+		};
+		}//end namespace OseenStep
 	}//end namespace NavierStokes
 } //end namespace Dune
 
