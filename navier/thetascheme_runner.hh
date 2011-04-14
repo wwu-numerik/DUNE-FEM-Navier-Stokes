3b6db9069d9d6676af73e78ea26ad6bd9694c662
Rene Milk <rene.milk@uni-muenster.de>
2011-04-14 09:32:58 +0200

    move scheme runner to own file

---
+++ navier/thetascheme_runner.hh
@@ -0,0 +1,91 @@
+#ifndef THETASCHEME_RUNNER_HH
+#define THETASCHEME_RUNNER_HH
+
+#include <dune/navier/thetascheme.hh>
+#include <dune/navier/thetascheme_alt_split.hh>
+
+template < class GridPartType, class CollectiveCommunicationType >
+class ThetaschemeRunner {
+	private:
+		typedef Dune::NavierStokes::ThetaSchemeTraits<
+						CollectiveCommunicationType,
+						GridPartType,
+						NAVIER_DATA_NAMESPACE::Force,
+						NAVIER_DATA_NAMESPACE::DirichletData,
+						NAVIER_DATA_NAMESPACE::Pressure,
+						NAVIER_DATA_NAMESPACE::Velocity,
+						1,//number of substeps
+						GridType::dimensionworld,
+						POLORDER,
+						VELOCITY_POLORDER,
+						PRESSURE_POLORDER >
+			OneStepThetaSchemeTraitsType;
+		typedef Dune::NavierStokes::ThetaScheme<OneStepThetaSchemeTraitsType>
+			OneStepThetaSchemeType;
+		typedef typename OneStepThetaSchemeTraitsType::ThetaSchemeDescriptionType
+			OneStepThetaSchemeDescriptionType;
+		typedef Dune::NavierStokes::ThetaSchemeTraits<
+						CollectiveCommunicationType,
+						GridPartType,
+						NAVIER_DATA_NAMESPACE::Force,
+						NAVIER_DATA_NAMESPACE::DirichletData,
+						NAVIER_DATA_NAMESPACE::Pressure,
+						NAVIER_DATA_NAMESPACE::Velocity,
+						3,//number of substeps
+						GridType::dimensionworld,
+						POLORDER,
+						VELOCITY_POLORDER,
+						PRESSURE_POLORDER >
+			ThreeStepThetaSchemeTraitsType;
+		typedef Dune::NavierStokes::ThetaScheme<ThreeStepThetaSchemeTraitsType>
+			ThreeStepThetaSchemeType;
+		typedef Dune::NavierStokes::ThetaSchemeAltSplitting<ThreeStepThetaSchemeTraitsType>
+			ThreeStepThetaSchemeAltSplittingType;
+		typedef typename ThreeStepThetaSchemeTraitsType::ThetaSchemeDescriptionType
+			ThreeStepThetaSchemeDescriptionType;
+	public:
+		ThetaschemeRunner( const GridPartType& grid_part, CollectiveCommunicationType& comm )
+			:grid_part_(grid_part),
+			comm_(comm)
+		{}
+
+		RunInfoTimeMap run(const int scheme_type)
+		{
+			const double dt_ = Parameters().getParam( "fem.timeprovider.dt", double(0.1) );
+			switch ( scheme_type ) {
+				case 1: return OneStepThetaSchemeType(grid_part_,
+													  OneStepThetaSchemeDescriptionType::forward_euler( dt_ ) )
+									.run();
+				case 2: return OneStepThetaSchemeType(grid_part_,
+													  OneStepThetaSchemeDescriptionType::backward_euler( dt_ ) )
+									.run();
+				case 3: return OneStepThetaSchemeType(grid_part_,
+													  OneStepThetaSchemeDescriptionType::crank_nicholson( dt_ ) )
+									.run();
+				case 4: if ( Parameters().getParam("old_timestep", false) )
+							return ThreeStepThetaSchemeAltSplittingType(grid_part_,
+													  ThreeStepThetaSchemeDescriptionType::fs0( dt_ ) )
+									.run();
+						else
+							return ThreeStepThetaSchemeType(grid_part_,
+													  ThreeStepThetaSchemeDescriptionType::fs0( dt_ ) )
+									.run();
+				default: Logger().Info() << "Using default value for theta scheme type\n";
+				case 5: if ( Parameters().getParam("old_timestep", false) )
+						return ThreeStepThetaSchemeAltSplittingType(grid_part_,
+												  ThreeStepThetaSchemeDescriptionType::fs1( dt_ ) )
+								.run();
+					else
+						return ThreeStepThetaSchemeType(grid_part_,
+												  ThreeStepThetaSchemeDescriptionType::fs1( dt_ ) )
+								.run();
+			}
+		}
+
+	private:
+		const GridPartType& grid_part_;
+		CollectiveCommunicationType& comm_;
+};
+
+
+#endif // THETASCHEME_RUNNER_HH
--- src/dune_navier_stokes.cc
+++ src/dune_navier_stokes.cc
@@ -77,7 +77,7 @@
 #include <dune/stuff/timeseries.hh>
 #include <dune/stuff/signals.hh>
 
-#include <dune/navier/thetascheme.hh>
+#include <dune/navier/thetascheme_runner.hh>
 
 #ifndef COMMIT
 	#define COMMIT "undefined"
@@ -247,77 +247,6 @@ int main( int argc, char** argv )
 	Logger().Dbg() << "\nRun from: " << commit_string << std::endl;
 	return err;
 }
-
-template < class GridPartType, class CollectiveCommunicationType >
-class ThetaschemeRunner {
-	private:
-		typedef Dune::NavierStokes::ThetaSchemeTraits<
-						CollectiveCommunicationType,
-						GridPartType,
-						NAVIER_DATA_NAMESPACE::Force,
-						NAVIER_DATA_NAMESPACE::DirichletData,
-						NAVIER_DATA_NAMESPACE::Pressure,
-						NAVIER_DATA_NAMESPACE::Velocity,
-						1,//number of substeps
-						GridType::dimensionworld,
-						POLORDER,
-						VELOCITY_POLORDER,
-						PRESSURE_POLORDER >
-			OneStepThetaSchemeTraitsType;
-		typedef Dune::NavierStokes::ThetaScheme<OneStepThetaSchemeTraitsType>
-			OneStepThetaSchemeType;
-		typedef typename OneStepThetaSchemeTraitsType::ThetaSchemeDescriptionType
-			OneStepThetaSchemeDescriptionType;
-		typedef Dune::NavierStokes::ThetaSchemeTraits<
-						CollectiveCommunicationType,
-						GridPartType,
-						NAVIER_DATA_NAMESPACE::Force,
-						NAVIER_DATA_NAMESPACE::DirichletData,
-						NAVIER_DATA_NAMESPACE::Pressure,
-						NAVIER_DATA_NAMESPACE::Velocity,
-						3,//number of substeps
-						GridType::dimensionworld,
-						POLORDER,
-						VELOCITY_POLORDER,
-						PRESSURE_POLORDER >
-			ThreeStepThetaSchemeTraitsType;
-		typedef Dune::NavierStokes::ThetaScheme<ThreeStepThetaSchemeTraitsType>
-			ThreeStepThetaSchemeType;
-		typedef typename ThreeStepThetaSchemeTraitsType::ThetaSchemeDescriptionType
-			ThreeStepThetaSchemeDescriptionType;
-	public:
-		ThetaschemeRunner( const GridPartType& grid_part, CollectiveCommunicationType& comm )
-			:grid_part_(grid_part),
-			comm_(comm)
-		{}
-
-		RunInfoTimeMap run(const int scheme_type)
-		{
-			const double dt_ = Parameters().getParam( "fem.timeprovider.dt", double(0.1) );
-			switch ( scheme_type ) {
-				case 1: return OneStepThetaSchemeType(grid_part_,
-													  OneStepThetaSchemeDescriptionType::forward_euler( dt_ ) )
-									.run();
-				case 2: return OneStepThetaSchemeType(grid_part_,
-													  OneStepThetaSchemeDescriptionType::backward_euler( dt_ ) )
-									.run();
-				case 3: return OneStepThetaSchemeType(grid_part_,
-													  OneStepThetaSchemeDescriptionType::crank_nicholson( dt_ ) )
-									.run();
-				case 4: return ThreeStepThetaSchemeType(grid_part_,
-													  ThreeStepThetaSchemeDescriptionType::fs0( dt_ ) )
-									.run();
-				default: Logger().Info() << "Using default value for theta scheme type\n";
-				case 5: return ThreeStepThetaSchemeType(grid_part_,
-													  ThreeStepThetaSchemeDescriptionType::fs1( dt_ ) )
-									.run();
-			}
-		}
-
-	private:
-		const GridPartType& grid_part_;
-		CollectiveCommunicationType& comm_;
-};
 
 RunInfoTimeMap singleRun(  CollectiveCommunication& mpicomm,
 					const int refine_level_factor, const int scheme_type )
