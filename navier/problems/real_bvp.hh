40660f97f70a618a31e697047bc1c7383c6adfc9
Rene Milk <rene.milk@uni-muenster.de>
2011-10-06 13:24:07 +0200

    BVP def

--- navier/problems/real_bvp.hh
+++ navier/problems/real_bvp.hh
@@ -20,6 +20,76 @@ static const bool hasExactSolution	= true;
     NULLFUNCTION_TP_BOUNDARY(DirichletData)
 }
 
+namespace BVP_A {
+
+static const std::string identifier = "BVP";
+static const bool hasExactSolution	= false;
+
+template < class FunctionSpaceImp, class TimeProviderImp >
+class DirichletData : public Dune::IntersectionTimeFunction < FunctionSpaceImp , DirichletData< FunctionSpaceImp,TimeProviderImp >, TimeProviderImp >
+{
+		public:
+			typedef DirichletData< FunctionSpaceImp, TimeProviderImp >
+				ThisType;
+			typedef Dune::IntersectionTimeFunction< FunctionSpaceImp, ThisType, TimeProviderImp >
+				BaseType;
+			typedef typename BaseType::DomainType
+				DomainType;
+			typedef typename BaseType::RangeType
+				RangeType;
+		/**
+		*  \brief  constructor
+		*
+		*  doing nothing besides Base init
+		**/
+		DirichletData( const TimeProviderImp& timeprovider,
+				   const FunctionSpaceImp& space,
+				   const double  /*viscosity*/ = 0.0,
+				   const double /*alpha*/ = 0.0 )
+			: BaseType ( timeprovider, space ),
+			z_max( Parameters().getParam( "z_max", 3.0 ) )
+		{}
+
+		/**
+		*  \brief  destructor
+		*
+		*  doing nothing
+		**/
+		~DirichletData()
+		{}
+		void evaluateTime( const double /*time*/, const DomainType& /*arg*/, RangeType& ret ) const
+		{
+		    ret = RangeType( 0 );
+		}
+
+		template < class IntersectionType >
+		void evaluateTime( const double time, const DomainType& arg, RangeType& ret, const IntersectionType& /*intersection */) const
+		{
+		    dune_static_assert( dim_ == 3 ,"__CLASS__ evaluate not implemented for world dimension");
+		    DomainType normal(0);
+		    normal[2] =  1 ;
+		    const double gd_factor = time * Parameters().getParam( "gd_factor", 1.0 );
+		    if ( arg[2] > 0.0 && arg[2] < z_max )
+		    {
+			normal *= 0.0;
+		    }
+		    ret = normal;
+		    ret *= gd_factor;
+		}
+
+	private:
+		  static const int dim_ = FunctionSpaceImp::dimDomain ;
+		  const double z_max;
+};
+
+NULLFUNCTION_TP(PressureGradient)
+NULLFUNCTION_TP(Pressure)
+NULLFUNCTION_TP(VelocityConvection)
+NULLFUNCTION_TP(VelocityLaplace)
+NULLFUNCTION_TP(Velocity)
+NULLFUNCTION_TP(Force)
+}
+
 namespace BVP {
 
 static const std::string identifier = "BVP";
@@ -78,18 +148,35 @@ static const bool hasExactSolution	= false;
 		    }
 
 		    template < class IntersectionType >
-		    void evaluateTime( const double time, const DomainType& arg, RangeType& ret, const IntersectionType& /*intersection */) const
+		    void evaluateTime( const double /*time*/, const DomainType& arg, RangeType& ret, const IntersectionType& intersection ) const
 		    {
-			dune_static_assert( dim_ == 3 ,"__CLASS__ evaluate not implemented for world dimension");
-			DomainType normal(0);
-			normal[2] =  1 ;
-			const double gd_factor = time * Parameters().getParam( "gd_factor", 1.0 );
-			if ( arg[2] > 0.0 && arg[2] < z_max )
-			{
-			    normal *= 0.0;
+			const int id = intersection.boundaryId();
+			switch ( id ) {
+			    case 1: {
+				ret[0] = 0.0;//arg[1];
+				ret[1] = 0.0;//-1.0;//arg[0];
+				ret[2] = 0.0;
+				return;
+			    }
+			    case 2: {
+				ret[0] = 1000.0;//arg[1];
+				ret[1] = 1000.0;//-1.0;//arg[0];
+				ret[2] = 1000.0;
+				return;
+			    }
+			    case 6:
+			    case 5:
+			    case 4:
+			    case 3: {
+				ret[0] = 1000.0;//arg[1];
+				ret[1] = 1000.0;//-1.0;//arg[0];
+				ret[2] = 1000.0;
+				return;
+			    }
+			    default:
+				assert( false );
+				return;
 			}
-			ret = normal;
-			ret *= gd_factor;
 		    }
 
 	    private:
@@ -101,7 +188,61 @@ static const bool hasExactSolution	= false;
     NULLFUNCTION_TP(Pressure)
     NULLFUNCTION_TP(VelocityConvection)
     NULLFUNCTION_TP(VelocityLaplace)
-    NULLFUNCTION_TP(Velocity)
+
+    template < class FunctionSpaceImp, class TimeProviderImp >
+    class Velocity : public Dune::TimeFunction < FunctionSpaceImp , Velocity< FunctionSpaceImp,TimeProviderImp >, TimeProviderImp >
+    {
+    public:
+	    typedef Velocity< FunctionSpaceImp, TimeProviderImp >
+		    ThisType;
+	    typedef Dune::TimeFunction< FunctionSpaceImp, ThisType, TimeProviderImp >
+		    BaseType;
+	    typedef typename BaseType::DomainType
+		    DomainType;
+	    typedef typename BaseType::RangeType
+		    RangeType;
+
+	    /**
+       *  \brief  constructor
+       *
+       *  doing nothing besides Base init
+       **/
+	    Velocity(	const TimeProviderImp& timeprovider,
+				    const FunctionSpaceImp& space,
+				    const double parameter_a = M_PI /2.0 ,
+				    const double parameter_d = M_PI /4.0)
+		    : BaseType( timeprovider, space ),
+		      lambda_( Parameters().getParam( "lambda", 0.0 ) )
+	    {}
+
+	    /**
+       *  \brief  destructor
+       *
+       *  doing nothing
+       **/
+	    ~Velocity()
+	    {}
+
+	    void evaluateTime( const double time, const DomainType& arg, RangeType& ret ) const
+	    {
+		    ret = arg;
+		    ret *= ( 1 + time);
+	    }
+
+	    /**
+       * \brief  evaluates the dirichlet data
+       * \param  arg
+       *         point to evaluate at
+       * \param  ret
+       *         value of dirichlet boundary data at given point
+       **/
+	    //					inline void evaluate( const DomainType& arg, RangeType& ret ) const {assert(false);}
+
+    private:
+	    static const int dim_ = FunctionSpaceImp::dimDomain ;
+	    const double lambda_;
+    };
+
     NULLFUNCTION_TP(Force)
 }//end ns
 }//end ns
